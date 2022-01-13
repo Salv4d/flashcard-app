71ccfabd35b7f8655c9dff1878221051234fe4b3
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-01-12 21:13:04 -0400

    add sample data to the game

---
+++ index.js
@@ -0,0 +1,42 @@
+const words = {
+  apple: {
+    image: "/images/apple.png",
+    text: { en: "Apple", ptbr: "Maçã" },
+  },
+  broccoli: {
+    image: "/images/broccoli.png",
+    text: { en: "Broccoli", ptbr: "Brócolis" },
+  },
+  carrot: {
+    image: "/images/carrot.png",
+    text: { en: "Carrot", ptbr: "Cenoura" },
+  },
+  corn: {
+    image: "/images/corn.png",
+    text: { en: "Corn", ptbr: "Milho" },
+  },
+  garlic: {
+    image: "/images/garlic.png",
+    text: { en: "Garlic", ptbr: "Alho" },
+  },
+  mushroom: {
+    image: "/images/mushroom.png",
+    text: { en: "Mushroom", ptbr: "Cogumelo" },
+  },
+  potato: {
+    image: "/images/potato.png",
+    text: { en: "Potato", ptbr: "Batata" },
+  },
+  pumpkin: {
+    image: "/images/pumpkin.png",
+    text: { en: "Pumpkin", ptbr: "Abóbora" },
+  },
+  tomato: {
+    image: "/images/tomato.png",
+    text: { en: "Tomato", ptbr: "Tomate" },
+  },
+  tree: {
+    image: "/images/tree.png",
+    text: { en: "Tree", ptbr: "Árvore" },
+  },
+};
